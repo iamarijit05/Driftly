2a9eedcfaff7c7d74773f5daffbf84c2eb6d3d13
ARIJIT DAS <iarijitdas03@gmail.com>
2026-07-23 22:50:12 +0530

    Add Footer, Testimonial, and Newsletter components; update Home and CarDetails pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home'
 import CarDetails from './pages/CarDetails'
 import Cars from './pages/Cars'
 import MyBookings from './pages/MyBookings'
+import Footer from './components/Footer'
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false)
@@ -16,12 +17,12 @@ const App = () => {
 
       <Routes>
         <Route path='/' element={<Home/>} />
-        <Route path='/car-details:id' element={<CarDetails/>} />
+        <Route path='/car-details/:id' element={<CarDetails/>} />
         <Route path='/cars' element={<Cars/>} />
         <Route path='/my-bookings' element={<MyBookings/>} />
       </Routes>
 
-
+      {!isOwnerPath &&  <Footer />}
     </>
   )
 }
---
+++ client/src/components/Testimonial.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import Title from './Title'
+import { assets } from '../assets/assets'
+
+const Testimonial = () => {
+    const testimonials = [
+        {
+            name: "Arijit Das",
+            location: "Kolkata, WB",
+            image: assets.testimonial_image_1,
+            testimonial: "I've rented cars from various companies, but the experience with Driftly was exceptional."
+        },
+        {
+            name: "Smita Banerjee",
+            location: "Delhi",
+            image: assets.testimonial_image_2,
+            testimonial: "Driftly made my trips much easier. The car was delivered right to my door, and the customer service was fantastic."
+        },
+        {
+            name: "Swarup Kumar",
+            location: "Mumbai, MH",
+            image: assets.testimonial_image_2,
+            testimonial: "I highly recommend Driftly! Their fleet is amazing, and I always feel like I'm getting the best deal with excellent service."
+        }
+    ]
+
+    return (
+        <div className="py-28 px-6 md:px-16 lg:px-24 xl:px-44">
+
+            <Title
+                title="What Our Customers Say"
+                subtitle="Discover why travelers choose Driftly for reliable cars and exceptional service."
+            />
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-18">
+                {testimonials.map((testimonial, index) => (
+                    <div
+                        key={index}
+                        className="bg-white p-6 rounded-xl shadow-lg hover:-translate-y-1 transition-all duration-300"
+                    >
+                        <div className="flex items-center gap-3">
+                            <img
+                                className="w-12 h-12 rounded-full"
+                                src={testimonial.image}
+                                alt={testimonial.name}
+                            />
+
+                            <div>
+                                <p className="text-xl">{testimonial.name}</p>
+                                <p className="text-gray-500">{testimonial.location}</p>
+                            </div>
+                        </div>
+
+                        <div className="flex items-center gap-1 mt-4">
+                            {Array(5).fill(0).map((_, index) => (
+                                <img
+                                    key={index}
+                                    src={assets.star_icon}
+                                    alt="star-icon"
+                                    className="w-4"
+                                />
+                            ))}
+                        </div>
+
+                        <p className="text-gray-500 max-w-90 mt-4 font-light">
+                            "{testimonial.testimonial}"
+                        </p>
+                    </div>
+                ))}
+            </div>
+
+        </div>
+    )
+}
+
+export default Testimonial
