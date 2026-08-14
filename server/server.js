20eef190876f22cd62cdce114126579bdffdc076
ARIJIT DAS <iarijitdas03@gmail.com>
2026-08-14 16:16:21 +0530

    implement user authentication with registration, login, and protected routes

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import "dotenv/config"
 import cors from "cors"
 import connectDB from './configs/db.js';
+import userRouter from './routes/userRoutes.js';
 
 //initialize express app
 const app = express()
@@ -14,6 +15,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => res.send("Server is Running"))
+app.use('/api/user', userRouter)
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is Running on port ${PORT}`))
