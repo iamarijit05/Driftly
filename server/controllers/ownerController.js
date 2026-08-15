2a3860215ee8b8055a5e9d009cdd252383175ac4
ARIJIT DAS <iarijitdas03@gmail.com>
2026-08-15 19:24:52 +0530

    add owner routes and integrate image upload functionality
    - Added new ownerRoutes.js for handling owner-related operations.
    - Implemented changeRoleToOwner and addCar endpoints with image upload support.
    - Updated server.js to include owner routes.
    - Enhanced package.json and package-lock.json with new dependencies: @imagekit/nodejs, imagekit, and multer.

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -1,2 +1,51 @@
+import User from "../models/User.js"
+import Car from '../models/Car.js'
+import fs from 'fs'
+import imagekit from "../configs/imageKit.js"
 
+// api to change role of user
+export const changeRoleToOwner = async(req, res) => {
+    try {
+        const {_id} = req.user
+        await User.findByIdAndUpdate(_id, {role: "owner"})
+        res.json({success: true, message: "Now You Can List Cars"})
+    } catch (error) {
+        console.log(error.message)
+        res.json({success: false, message: error.message})
+    }
+}
+
+//API to list car
+export const addCar = async(req, res) => {
+    try {
+        const {_id} = req.user
+        let car = JSON.parse(req.body.CarData)
+        const imageFile = req.file;
+        
+        const fileBuffer = fs.readFileSync(imageFile.path)
+        const response = await imagekit.upload({
+            file: fileBuffer,
+            fileName: imageFile.originalname,
+            folder: '/cars'
+        })
+
+        //optimization through imagekit url transformation
+
+        var optimizedImageUrl = imagekit.url({
+            path: response.filePath,
+            transformation : [
+                {width: '1280'}, // width resizing
+                {quality: 'auto'}, // auto compression
+                { format: 'webp'} // convert to modern format
+            ]
+        })
+        const image = optimizedImageUrl
+        await Car.create({...car, owner: _id, image})
+        res.json({success: true, message: "Car Added"})
+        
+    } catch (error) {
+        console.log(error.message)
+        res.json({success: false, message: error.message})
+    }
+}
 
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import "dotenv/config"
 import cors from "cors"
 import connectDB from './configs/db.js';
 import userRouter from './routes/userRoutes.js';
+import ownerRouter from './routes/ownerRoutes.js';
 
 //initialize express app
 const app = express()
@@ -16,6 +17,7 @@ app.use(express.json())
 
 app.get('/', (req, res) => res.send("Server is Running"))
 app.use('/api/user', userRouter)
+app.use('/api/owner', ownerRouter)
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is Running on port ${PORT}`))
